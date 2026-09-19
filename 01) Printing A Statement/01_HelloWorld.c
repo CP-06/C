6b7e3861b065ddb852cf65c3a07fcc18f43bd934
CP-06 <praveen_c2k6@outlook.com>
2026-09-20 00:52:54 +0530

    Add files via upload

---
+++ 01) Printing A Statement/01_HelloWorld.c
@@ -0,0 +1,6 @@
+# include <stdio.h> // 'stdio.h' is a pre-processor directive. '# include <stdio.h>' is known as header file
+int main() // execution of any program starts from the main method
+{ // start of the main method
+    printf("Hello World !"); // statement 
+    return 0; // marks the end of the main program 
+} // end of the main method
---
+++ 01) Printing A Statement/02_Details.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main()
+{
+    printf("Name : C Praveen\n"); // '\n' is the escape sequence, it moves the cursor to the new line 
+    printf("Age : 20 years\n");
+    printf("Date of birth : 6th October 2006\n");
+    return 0;
+}
